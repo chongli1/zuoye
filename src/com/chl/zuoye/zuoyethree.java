b26c587f52d884a4403a8657e40c44723d54a399
chl <3170774891@qq.com>
2021-07-13 18:46:44 +0800

    4、第四题完成

---
+++ src/com/chl/zuoye/zuoyethree.java
@@ -0,0 +1,29 @@
+package com.chl.zuoye;
+
+import java.util.Scanner;
+
+public class zuoyethree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数：");
+        String num = sc.nextLine();
+        String str1 = num + "";
+
+        StringBuilder str2 = new StringBuilder(str1);
+        str2.reverse();
+        int count = 0;
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                System.out.println(str1 + "不是回文数");
+                break;
+            } else {
+                count++;
+            }
+        }
+        if (count == str1.length()) {
+            System.out.println(str1 + "是回文数");
+        }
+    }
+
+}
